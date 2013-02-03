8324038b27e5df95248e3be536e94a594fea5927
bigohh <sweetdevansh@gmail.com>
2013-02-03 05:18:58 -0800

    Create in.js

---
+++ in.js
@@ -0,0 +1,10 @@
+var item=["car","car","car","car","car","car","car","car","car","car","car","car","car","car","car","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","sports","cartoons","cartoons","cartoons","cartoons","cartoons","cartoons","cartoons","cartoons","food","food","food","food","food","food","food","food"]
+
+
+var names=["AUDI","BENTELY","CHEVROLET","DODGE","FORD","HYUNDAI","HONDA","TATA","TOYOTA","MAZDA","OPEL","PORCHE","ROLLSROYCE","VOLKSWAGEN","JAGUAR","ADIDAS","CHELSEA","ARSENAL","MANCHESTERUNITED","BARCELONA","REALMADRID","BAYERNMUNICH","LIVERPOOL","ACMILAN","DECCANCHARGERS","PUNEWARRIORS","CHENNAISUPERKINGS","MUMBAIINDIANS","KINGSXIPUNJAB","ITALY","SPAIN","ARGENTINA","MICKEYMOUSE","GARFIELD","DONALD","ED","POOH","POWERPUFFGIRLS","ROADRUNNER","TWEETY","AMUL","BASKINROBINS","MCDONALDS","KFC","DOMINOS","KNORR","NESTLE","SUBWAY"]
+
+
+
+var arr_1=["audi.jpg","bentely.jpg","chevrolet.jpg","dodge.jpg","ford.jpg","hyundai.jpg","honda.jpg","tata.jpg","toyota.jpg","mazda.jpg","opel.jpg","porche.jpg","rolls.jpg","volkswagen.jpg","jaguar.jpg","adidas.jpg","chelsea.jpg","arsenal.jpg","manchester.jpg","barcelona.jpg","real.jpg","bayern.jpg","liverpool.jpg","acmilan.jpg","deccan.jpg","pune.jpg","chennai.jpg","mumbai.jpg","kings.jpg","italy.jpg","spain.png","argentina.jpg","mickey.jpg","garfield.jpg","donald.jpg","ed.jpg","pooh.jpg","power.jpg","road.jpg","tweety.jpg","amul.jpg","br.jpg","dominos.jpg","images.jpg","kfc.jpg","domino.jpg","knorr.jpg","nestle.jpg","subway.jpg"]
+
+
